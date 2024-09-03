5db7d5c1d57b94217a2574f76324d93d250c6f94
dglemos <dlemos@ebi.ac.uk>
2024-09-03 14:35:37 +0000

    Update API to filter pf by dna_type

--- modules/Bio/EnsEMBL/Variation/DBSQL/PhenotypeFeatureAdaptor.pm
+++ modules/Bio/EnsEMBL/Variation/DBSQL/PhenotypeFeatureAdaptor.pm
@@ -117,6 +117,23 @@ sub _is_significant_constraint {
 }
 
 
+=head2 _filter_dna_type
+
+=cut
+
+sub _filter_dna_type {
+  my $self = shift;
+  my $constraint = shift;
+  my $dna_type = shift;
+
+  # ClinVar phenotype feature has DNA_type 'Somatic' or 'Germline'
+  # For other human sources or non-human species the DNA_type is null
+  my $dna_constraint = qq{ (pf.DNA_type='$dna_type' OR pf.DNA_type is NULL) };
+  $constraint .= (defined($constraint)) ? " AND$dna_constraint" : $dna_constraint;
+
+  return $constraint;
+}
+
 =head2 _is_class_constraint
 
   Arg [1]    : string $constraint
@@ -153,7 +170,8 @@ sub _fetch_all_by_object {
   my $self   = shift;
   my $object = shift;
   my $type   = shift;
-  
+  my $dna_type = shift; # dna_type to include
+
   $type ||= (split '::', ref($object))[-1];
   throw("$type is not a valid object type, valid types are: ".(join ", ", sort keys %TYPES)) unless defined $type and defined($TYPES{$type});
   
@@ -163,7 +181,12 @@ sub _fetch_all_by_object {
   $constraint = $self->_is_significant_constraint($constraint);
   # Add the constraint for phenotype class
   $constraint = $self->_is_class_constraint($constraint);
-     
+
+  # Filter phenotype features by their DNA_type
+  if(defined $dna_type) {
+    $constraint = $self->_filter_dna_type($constraint, $dna_type);
+  }
+
   return $self->generic_fetch($constraint);
 }
 
@@ -416,7 +439,20 @@ sub fetch_all_by_Variation {
     throw('Bio::EnsEMBL::Variation::Variation arg expected');
   }
   
-  return $self->_fetch_all_by_object($var, 'Variation');
+  # Only include phenotype features with DNA_type 'Germline' or null
+  return $self->_fetch_all_by_object($var, 'Variation', 'Germline');
+}
+
+sub fetch_all_somatic_by_Variation {
+  my $self = shift;
+  my $var  = shift;
+
+  if(!ref($var) || !$var->isa('Bio::EnsEMBL::Variation::Variation')) {
+    throw('Bio::EnsEMBL::Variation::Variation arg expected');
+  }
+
+  # Only include phenotype features with DNA_type 'Somatic' or null
+  return $self->_fetch_all_by_object($var, 'Variation', 'Somatic');
 }
 
 
